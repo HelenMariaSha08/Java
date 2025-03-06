1b53d9572dcaedc6d631b18e2e1b1a1ef7555004
Helen Maria Sha <helenmsha08@gmail.com>
2025-03-06 15:49:57 +0530

    Palindrome

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class palindrome{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the word");
+        String st=sc.nextLine();
+        String rev="";
+        for (int i=st.length()-1;i>=0;i--){
+            rev=rev+st.charAt(i);
+        }
+        if(rev.equalsIgnoreCase(st)){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+    }
+}
