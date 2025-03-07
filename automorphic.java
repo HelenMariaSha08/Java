7dfd14d4d81384a6531728782bdbc9678215cc57
Helen Maria Sha <helenmsha08@gmail.com>
2025-03-07 15:16:35 +0530

    Automorphic

---
+++ automorphic.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class automorphic{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n=sc.nextInt();
+        int ncpy=n;
+        int cnt=0;
+        while(n!=0){
+            n=n/10;
+            cnt++;
+        }
+        int sq=ncpy*ncpy;
+        int sqcpy=sq;
+        int l=sq%((int)Math.pow(10,cnt));
+        if(l==ncpy){
+            System.out.println("Automorphic \nThe number is "+ncpy+"the square"+sqcpy);
+        }
+        else{
+            System.out.println("Not Automorphic \nThe number is"+ncpy+"The square is"+sqcpy);
+        }
+        
+    }
+}
