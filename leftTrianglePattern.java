6e15f51a510161a98d89edb4fdd074b5caa70a6a
Helen Maria Sha <helenmsha08@gmail.com>
2025-03-07 10:49:25 +0530

    left triangle pattern

---
+++ leftTrianglePattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class leftTrianglePattern{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of rows");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=n-1;j>=n-i-1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    
+    }   
+}
