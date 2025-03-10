445d116c4bdf31d4271f0b7b4367eda8e85890e0
Helen Maria Sha <helenmsha08@gmail.com>
2025-03-10 13:29:22 +0530

    Reverse an Array

---
+++ reverse.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.Arrays;  
+public class reverse {  
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of elements");
+        int n=sc.nextInt();
+        System.out.println("enter elements of array");
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        
+            
+            
+        
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+            
+        }
+        
+    }
+}  
