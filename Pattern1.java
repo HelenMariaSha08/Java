57c7070d38c619617e4b95e1b96c472ad9513cb0
Helen Maria Sha <helenmsha08@gmail.com>
2025-03-10 14:42:25 +0530

    Pattern1

---
+++ Pattern1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.Arrays;  
+public class Pattern1 {  
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of elements");
+        int n=sc.nextInt();
+        System.out.println("enter elements of array");
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=(n-j-1)){
+                    System.out.print(arr[j]+" ");
+                    
+                }
+                else{
+                    System.out.print("* ");
+                }
+            
+                
+            }
+            System.out.println();
+        }
+        
+    }
+}  
